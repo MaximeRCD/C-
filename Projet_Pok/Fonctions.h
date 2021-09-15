4cc8e49ebc18f582140488e4e176ab52989dea3e
Maxime RICHAUDEAU <maxime.richaudeau1@gmail.com>
2021-09-15 17:38:28 +0200

    Projet Pokemon C++

---
+++ Projet_Pok/Fonctions.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Structure.h"
+#include <cmath>
+#include <cstdlib>
+#include<stdlib.h>
+#include<time.h>
+
+// initialisation du bestiaire
+void init_bestiaire(especePokemon bestiaire[]);
+// tri du bestiaire
+void tri(especePokemon bestiaire[]);
+//on cherche le type des pokemons sous la forme d'un string 
+std::string cherchertypepok(especePokemon bestiaire[], std::string pokemoncherche, std::string* tab);
+// créer un tableau dyn de taille size
+pokemon* initTableauDynPokedex(const int size);
+// initialise la structure pokedex
+void initPokedex(Pokedex* po);
+// insert un pokemon dans le tableau dyn
+void insertPokemon(Pokedex* po, pokemon* p);
+// créer un entier aléatoire
+long random_at_most(long max);
+// initialisation d'un pokémon sauvage
+void init_pokemon(pokemon* p, std::string name, int exp, int pdv, int niv, int cp);
+// génération d'un pokemon sauvage
+pokemon genererPokemon(especePokemon* bestiaire);
+// augmentation des ressources après la capture
+void nouv_ress(ressources* p);
+// fonction powerUp
+void powerUp(pokemon* p, ressources* r);
+// fonction evolution 
+void evolve(pokemon* p, especePokemon* bestiaire, ressources* r, historique* mon_historique);
+// fonction insert evolution pour l'historique
+void insert_evolution(historique* mon_historique, std::string from, std::string to);
+
+Historique* initialisation();
+
+// focntion de hashage
+int hashfct(std::string str, int max);
+
+// get alveole
+CombatEspece* getalveole(HashTable* myHashtable, std::string key);
+// la table contient il cette clé
+bool contains(HashTable* myHashtable, CombatEspece* c);
+// insert table de hachage
+void insert(CombatEspece* c, HashTable* myHashtable);
+//init HT
+HashTable* initHashTable(HashTable* hashtable);
+// combat
+void combat(Pokedex* po, especePokemon* bestiaire, std::string* tab, HashTable* myHashtable);
---
+++ Projet_Pok/Structure.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <string>
+#include "Structure.h"
+// le type enuméré décrivant le type de Pokemon parmi les 18 existants
+enum PokemonType
+{
+    Normal, Combat, Flying, Poison, Sol, Roche, Bug,
+    Fantome, Steel, Feu, Herbe, Eau, Electrique, Psychique,
+    Glace, Dragon, Dark, Fairy
+};
+//on créer la structure pokemon
+typedef struct pokemon{ // typedef permet de ne pas répéter struct pokemeon à chaque fois que l'on en a besoin ailleurs.
+	std::string nom; //nom du pokémon
+	int xp;   //son nombre d'xp
+	int pv;   //son nbr de pv
+	int niv;  //son numéro d'évolution
+	int cp;   //son nbr de points de combats
+}pokemon;
+// je créé un nouveau type qui est struct especepokemon dont l'alias est especePokemon
+typedef struct especePokemon{
+	std::string nom;             // dans le .h on utilise pas using namespace std donc il faut le mettre
+	PokemonType type;
+	std::string evolution;
+	unsigned int nb_bonbon_to_evolv;
+	bool estEvolue;
+}especePokemon;
+//on créer la structure des ressources du joueur
+typedef struct ressources {
+	int bonbons;
+	int poussieres;
+}ressources;
+// on créer la structure pokedex
+typedef struct Pokedex {
+	// pointeur vers tableau dynamique de pokemon
+	pokemon* mesPokemons;
+	int capacite;
+	int nbPokemon;
+}Pokedex;
+
+typedef struct Evolution {
+	std::string ev_from;
+	std::string ev_to;
+	Evolution* suivant;
+}evolution;
+typedef struct Historique {
+	int nombre;
+	Evolution* debut;
+}historique;
+
+typedef struct CombatEspece {
+	std::string nature;
+	std::string strongAgainst[5];
+	std::string weakAgainst[5];
+	CombatEspece* next;
+}combatEspece;
+
+typedef struct Head {
+	int nbr_espece;
+	int hash;
+	CombatEspece* first;
+}head;
+
+typedef struct HashTable {
+	/*Le facteur de compression (load factor) qui est la proportion d'alvéoles utilisées dans une table de hachage est une indication critique de ses performances.
+		Il est défini ainsi :
+								facteur de compression = k/n
+	k est le nombre de paires clé–valeur ;
+	n est le nombre d'alvéoles.
+	Dans notre cas k=18 et fact_comp=0,7 donc  n=26
+	*/
+	int nbr_ele;
+	int nbr_alveole;
+	Head** tab; // tableau de pointeur
+}hashTable;
+
